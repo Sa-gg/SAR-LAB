c2b0427ef0d6bfe65ce77b9e121898f2f6a7c5cb
Patrick Sagum <sagum.patrick.r@gmail.com>
2026-03-17 18:12:26 +0800

    Isolate lab1 branch to lab1-only content

--- lab3/server/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/orderController');
-
-router.get('/products', ctrl.getAllProducts);
-router.get('/products/:id', ctrl.getProductById);
-router.post('/orders', ctrl.placeOrder);
-
-module.exports = router;
